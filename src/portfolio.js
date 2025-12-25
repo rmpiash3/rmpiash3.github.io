b342451db81dbf8ee6660178d36213058f3a4ea2
Riad Mahamud Piash <riadmahmudpiash@gmail.com>
2025-12-25 22:01:05 +0600

    Remove redirect meta tag

--- src/portfolio.js
+++ src/portfolio.js
@@ -46,7 +46,7 @@ const socialMediaLinks = [
   },
   {
     name: "Facebook",
-    link: "https://www.facebook.com/tasinRiad Mahamud Piash",
+    link: "https://www.facebook.com/rmpiash3/",
     fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
     backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
   },
